e25611ceb57f9f21a8abdc6e705f2840ba489367
yaroslav <ylatushkov@icloud.com>
2022-11-01 14:10:36 +0300

    Initial commit

---
+++ src/test/java/mullinli/SpringPetBot/SpringPetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package mullinli.SpringPetBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
